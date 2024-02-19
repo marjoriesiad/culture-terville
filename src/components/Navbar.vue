2f8bcad23d958a7140cd146aa0590b7dd7d0e9ce
Marjorie Siad <marjoriesiad@gmail.com>
2024-02-19 16:33:57 +0100

    init project; cleanup & navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-red-700 text-black flex justify-between items-center p-2 px-4 uppercase">
+        <div class="flex flex-col text-center font-bold">
+            <a href="/">Culture Pain</a>
+            <p class="text-sm text-white tracking-widest">Terville</p>
+        </div>
+        
+        <div class="flex space-x-4 font-bold">
+            <a href="/">Menu</a>
+            <a href="/">Contacts</a>
+        </div>
+    </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePageVue from '@/views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePageVue
+    },
+    
+  ]
+})
+
+export default router
